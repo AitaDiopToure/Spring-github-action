318ad2551843b081bb04ba08faa5d419f57da773
aitadioptoure <aitoussha@gmail.com>
2023-01-08 19:51:30 +0000

    first commit

---
+++ src/main/java/com/groupeisi/projet/autentification/ProjetAutentificationApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.projet.autentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAutentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAutentificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeisi/projet/autentification/ProjetAutentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.projet.autentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAutentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
